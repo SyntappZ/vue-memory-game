0729af3b1148e2fe60cd04725fb3b3eb96baf169
apex viruz <doddsanater87@gmail.com>
2019-03-30 20:09:25 +0000

    finished most of the game

--- script.js
+++ script.js
@@ -6,64 +6,88 @@ window.onload = () => {
          title: 'Vue Memory Game',
          message: '',
          name: 'BiscuitmanZ 2019',
-         points: 10,
+         points: 0,
          turnedArr: [],
-         winners: [],
+         ids: [],
          pack: [],
+         over: false,
          start: false,
-         i: 1
-     
+         winner: false,
      },
     methods: {
       makeRandomArray() {
          this.start = true;
+         this.points = 5;
          let arr1 = []
          let arr2 = []
          let letters = ['X', 'B', 'G', 'E', 'O', 'Y']
+
          for(let i = 0; i < 6; i++) {
             arr1.push({id:i, face: letters[i], show: false, won: false})
             arr2.push({id:6 + i, face: letters[i], show: false, won: false})
          }
-         // () => Math.floor(Math.random() * 3 - 1)
-        let joinArrs = arr1.concat(arr2).sort()
+         
+        let joinArrs = arr1.concat(arr2).sort(() => Math.floor(Math.random() * 3 - 1))
         
         this.pack = joinArrs;
         
      },
          turnOver(card) {
-             card.show = true
-            if(this.turnedArr.length < 2) {
-              
-               this.turnedArr.push({id:card.id, face:card.face, show:card.show, won:card.won});
-            }
-            if(this.turnedArr.length > 1){
-               if(this.turnedArr[1].face === this.turnedArr[0].face){
-                  this.turnedArr[1].win = true;
-                  this.turnedArr[0].win = true;
-                  this.winners.push(this.turnedArr)
-                  this.turnedArr = this.winners
-                  
-                  console.log(this.winners)
-               }else if(this.turnedArr[1].face != this.turnedArr[0].face){
-                 
-                 
-                 setTimeout(() => {
-                 this.pack.forEach(e => e.won ? e.show = true : e.show = false)
+            card.show = true
+            this.ids.length < 2 ? this.ids.push(card.id) : null
+            this.turnedArr.push({id:card.id, face:card.face, show:card.show, won:card.won});
 
-                 this.turnedArr = []
-                  console.log(this.turnedArr.length)
-                 }, 1000);
-               }
-               
+
+         if(this.turnedArr.length > 1) {
+            if(this.turnedArr[0].face === this.turnedArr[1].face) {
+               this.pack.forEach(e => {
+                  e.id === this.ids[0] ? e.won = true : null
+                  e.id === this.ids[1] ? e.won = true : null
+
+                  
+               })
+
+              this.turnedArr = []
+              this.ids = []
+              this.points++
+              this.match;
+
+              setTimeout(() => {
+               this.title = 'Vue Memory Game';
+            }, 2000);
+
+            }else{
+               this.points--
+               setTimeout(() => {
+                  this.pack.forEach(e => e.won ? e.show = true : e.show = false)
+                  this.points <= 0 ? this.endGame : null
+                  this.turnedArr = []
+                  this.ids = []
+                  
+               }, 600);
+               this.points <= 0 ? this.gameOver() : null
             }
-            
+         }
+         
+   
+      },
+      gameOver() {
+         this.pack.forEach(e => {
+            setTimeout(() => {
+               e.show = true;
+            }, 500);
            
-            
-            
-         },
+         })
+         this.over = true;
+      },
+        
+      restart() {
+        location.reload();
+      },
+         
          
         isDisabled(card) {
-         if(card.show || this.turnedArr.length === 2) {
+         if(card.show || this.turnedArr.length >= 2 || this.over) {
             return true
          }
       }
@@ -76,7 +100,23 @@ window.onload = () => {
         
      },
      computed: {
-      
+         endGame() {
+            return this.title = 'GAME OVER!'
+            
+         },
+         match() {
+            return this.title = 'Correct!'
+         },
+         winOrLoss() {
+            if(over) {
+               return 'Game Over!'
+            }
+            else if(winner) {
+               return 'Winner!'
+            }
+         }
+         
+         
      }
     
     
